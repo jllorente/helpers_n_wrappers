031103e75f62484c7a4f83bcd88cd362685149ca
Jesus Llorente Santos <jesus.llorente@gmail.com>
2018-03-13 11:24:29 +0200

    Modified nfqueue3.py to bind to several queues and display packet counter upon termination

--- nfqueue3.py
+++ nfqueue3.py
@@ -13,6 +13,8 @@ class NFQueue3(object):
         self.logger.info('Bind queue #{}'.format(queue))
         self._loop = asyncio.get_event_loop()
         self.queue = queue
+        # Use packet counter
+        self.counter = 0
         # Create NetfilterQueue object
         self._nfqueue = netfilterqueue.NetfilterQueue()
         # Bind to queue and register callbacks
@@ -27,9 +29,10 @@ class NFQueue3(object):
         self._loop.add_reader(self._nfqueue_fd, cb2)
 
     def _nfcallback(self, pkt):
+        self.counter += 1
         if self.cb is None:
             data = pkt.get_payload()
-            self.logger.info('Received ({} bytes): {}'.format(len(data), data))
+            self.logger.debug('Received ({} bytes): {}'.format(len(data), data))
             pkt.accept()
             return
 
@@ -43,7 +46,7 @@ class NFQueue3(object):
         self.cb_kwargs = cb_kwargs
 
     def terminate(self):
-        self.logger.info('Unbind queue #{}'.format(self.queue))
+        self.logger.info('Unbind queue #{}: received {} pkts'.format(self.queue, self.counter))
         self._loop.remove_reader(self._nfqueue_fd)
         self._nfqueue.unbind()
 
@@ -54,13 +57,17 @@ if __name__ == '__main__':
     ch = logging.StreamHandler(sys.stdout)
     ch.setFormatter(format)
     log.addHandler(ch)
-    log.setLevel(logging.DEBUG)
+    log.setLevel(logging.INFO)
     # Instantiate loop
     loop = asyncio.get_event_loop()
-    # Create NFQueue3 object
-    nfqueue = NFQueue3(int(sys.argv[1]), None)
+    # Create NFQueue3 objects
+    queues = []
+    for n in sys.argv[1:]:
+        queues.append(NFQueue3(int(n), None))
     try:
         loop.run_forever()
     except:
-        nfqueue.terminate()
+        pass
+    for q in queues:
+        q.terminate()
     loop.close()
